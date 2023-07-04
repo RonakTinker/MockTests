e72ea48f72addaf5735d7ecfedb666ac8fcb37e8
RonakTinker <ronaktinker243@gmail.com>
2023-07-04 17:55:55 +0530

    Problem1 solved

---
+++ Module Test1/src/in/ineuron/problems/Problem1.java
@@ -0,0 +1,19 @@
+package in.ineuron.problems;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+
+		int num = 0;
+
+		if (num == 0) {
+			System.out.println("the number is zero");
+		} else if (num < 0) {
+			System.out.println("number is negative");
+		} else {
+			System.out.println("number is positive");
+		}
+
+	}
+
+}
